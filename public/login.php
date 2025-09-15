91632e07ce5336430d2c91714681773c53c5d6f3
netow2774-lgtm <netow2774@gmail.com>
2025-09-15 10:58:43 -0400

    icon

--- public/login.php
+++ public/login.php
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Login - Compre Fácil</title>
+    <link rel="icon" href="../img/CompreFacil.png" type="image/png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../assets/style/animated-gradient.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
